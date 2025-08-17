2175a43837bdcf827c1316ec23e02bb64ef04d29
Stephen Weistra <vengefire@gmail.com>
2025-08-17 12:04:45 +0800

    Moved dependencies to solution folder Adjusted project settings to use moved dependencies resources.

--- learnOpenGL/main.cpp
+++ learnOpenGL/main.cpp
@@ -12,10 +12,9 @@ void ProcessInput(GLFWwindow* window)
 
 int main()
 {
-  glfwInit();
-
   try
   {
+    glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
--- learnOpenGL/vertex.glsl
+++
@@ -1,8 +0,0 @@
-#version 330 core
-layout (location = 0) in vec3 aPos; // Vertex position)
-
-void main()
-{
-	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); // Set the position of the vertex
-	// gl_Position = vec4(aPos, 1.0); // Set the position of the vertex
-}
